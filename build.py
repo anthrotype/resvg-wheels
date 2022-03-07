3a53d6e9e7faa7a92ec2c7b3a56791215ea731ca
Cosimo Lupo <clupo@google.com>
2022-03-07 15:16:10 +0000

    copy README.rst to the sdist, relative path outside root dir doesn't work: ../README.rst

--- build.py
+++ build.py
@@ -13,12 +13,16 @@ crate_dir = root_dir / "resvg"
 dist_dir = root_dir / "dist"
 cargo_manifest_path = crate_dir / "Cargo.toml"
 pyproject_toml_path = root_dir / "pyproject.toml"
+py_readme = root_dir / "README.rst"
 
 
 def main():
     # maturin expects the 'pyproject.toml' file to be located in the same
     # directory as Cargo.toml, so we copy it there
     shutil.copyfile(pyproject_toml_path, crate_dir / "pyproject.toml")
+    # We also want to include our own README.rst in the source distribution,
+    # alongside the original README.md
+    shutil.copyfile(py_readme, crate_dir / "README.python.rst")
 
     # resvg's top-level Cargo.toml declares a workspace with explicit members
     # (subfolders) and each members' Cargo.toml also explicitly points back to the

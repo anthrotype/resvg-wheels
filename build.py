e920b141a9cd9a0ecbadbcb89012c660435faf41
Cosimo Lupo <clupo@google.com>
2024-10-01 14:19:48 +0100

    ensure custom .cargo/config.toml gets included in the sdist

--- build.py
+++ build.py
@@ -13,6 +13,7 @@ crate_dir = root_dir / "resvg"
 dist_dir = root_dir / "dist"
 cargo_manifest_path = crate_dir / "Cargo.toml"
 pyproject_toml_path = root_dir / "pyproject.toml"
+cargo_config_path = root_dir / ".cargo" / "config.toml"
 py_readme = root_dir / "README.rst"
 
 
@@ -20,6 +21,9 @@ def main():
     # maturin expects the 'pyproject.toml' file to be located in the same
     # directory as Cargo.toml, so we copy it there
     shutil.copyfile(pyproject_toml_path, crate_dir / "pyproject.toml")
+    # also copy our custom .cargo/config.toml so it gets included in the sdist
+    (crate_dir / ".cargo").mkdir(exist_ok=True)
+    shutil.copyfile(cargo_config_path, crate_dir / ".cargo" / "config.toml")
     # We also want to include our own README.rst in the source distribution,
     # alongside the original README.md
     shutil.copyfile(py_readme, crate_dir / "README.python.rst")
